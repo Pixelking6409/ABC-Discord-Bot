d755d9ee07c1eefaaa62013a89fabd32ef7540a2
Pixel <53318360+Pixelking6409@users.noreply.github.com>
2022-07-07 11:49:29 +1200

    fix

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -8,12 +8,13 @@ module.exports = {
         const WelcomeEmbed = new MessageEmbed()
             .setTitle("Welcome to the ABC Discord Bot.")
             .setDescription("Somethings to do before starting.")
-            .addField("`-prefix`", "Use this to set a prefix default is `!`", true)
+            .addField("`-prefix`", `Use this to set a prefix default is \`${client.prefix}\``, true)
             .addField("`-setchannel`", "Set a channel to start using the bot!", true)
             .setTimestamp()
             .setFooter("Thank you for using this bot")
+            .setColor("RED")
 
-        var guildData = new ServerInfo({
+        const guildData = await ServerInfo.create({
             GuildId: guild.id,
             Words: [],
             Correct: 0,
