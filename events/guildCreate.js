a4c1390fc00d468bb32683c652d66185b234d3e7
Pixel <53318360+Pixelking6409@users.noreply.github.com>
2022-07-07 11:43:43 +1200

    fix?

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,7 +4,7 @@ const ServerInfo = require("../schema/ServerInfo");
 module.exports = {
     name: 'guildCreate',
     execute(guild, client) {
-        const channel = guild.channels.cache.first()
+        const channel = guild.channels.cache.filter(chx => chx.type === "GUILD_TEXT").find(x => x.position === 0);
         const WelcomeEmbed = new MessageEmbed()
             .setTitle("Welcome to the ABC Discord Bot.")
             .setDescription("Somethings to do before starting.")
