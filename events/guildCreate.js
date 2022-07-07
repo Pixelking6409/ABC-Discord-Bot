ae0c02a0c2afe940b4cc4fac12edeb1ae9025ca7
Pixel <53318360+Pixelking6409@users.noreply.github.com>
2022-07-07 12:26:23 +1200

    in a bag

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -14,16 +14,23 @@ module.exports = {
             .setFooter({ text: "Thank you for using this bot" })
             .setColor("RED")
 
-        const guildData = await ServerInfo.create({
-            GuildId: guild.id,
-            Words: [],
-            Correct: 0,
-            Restarts: 0,
-            BotChannel: 0,
-            Prefix: "-"
-        })
+        let guildExist = await ServerInfo.exists({ GuildId: guild.id }); 
 
-        console.log(guild.id + " data created!")
+        if (guildExist) {
+            console.log("Guild already exists!")
+        } else {
+            const guildData = await ServerInfo.create({
+                GuildId: guild.id,
+                Words: [],
+                Correct: 0,
+                Restarts: 0,
+                BotChannel: 0,
+                Prefix: "-"
+            })
+    
+            console.log(guild.id + " data created!")
+        }
+
         channel.send({ embeds: [WelcomeEmbed] })
     }
 }
