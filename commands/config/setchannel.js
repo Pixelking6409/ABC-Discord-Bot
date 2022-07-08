c0595c0bc53d49be5793ff73d697ba1dd8d7db1e
Pixel <53318360+Pixelking6409@users.noreply.github.com>
2022-07-08 14:58:49 +1200

    Ong

--- commands/config/setchannel.js
+++ commands/config/setchannel.js
@@ -10,7 +10,19 @@ module.exports = {
 
     async execute(message, args, client) {
 
-        let newBotChannel = await message.guild.channels.cache.get(args[0].substring(2).substring(0,18)) || message.channel.id
+        try {
+            let newBotChannel = await message.guild.channels.cache.get(args[0].substring(2).substring(0,18))
+        } catch(e) {
+            const ErrorEmbed = new MessageEmbed()
+                .setTitle(`❌ Please mention the channel`)
+                .setFooter({ text: `Requested by ${message.author.username}`, iconURL: message.author.displayAvatarURL() })
+                .setColor("RED")
+                .setTimestamp()
+
+            return message.reply({ embeds: [ErrorEmbed] })
+        }
+
+        console.log(newBotChannel)
         let GuildInfo = await ServerInfo.findOneAndUpdate({ GuildId: message.guild.id }, { BotChannel: newBotChannel });
 
         const SuccessEmbed = new MessageEmbed()
